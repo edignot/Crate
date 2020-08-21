662fbcd44b2fed3d35d0cca83cc8c56d31e047d5
Ed Ignot <edignot@gmail.com>
2020-08-20 19:58:23 -0600

    Graphql schema annotation

--- code/api/src/setup/schema/index.js
+++ code/api/src/setup/schema/index.js
@@ -1,14 +1,20 @@
 // Imports
-import { GraphQLSchema } from 'graphql'
+import { GraphQLSchema } from 'graphql';
 
 // App Imports
-import query from './queries'
-import mutation from './mutations'
+import query from './queries';
+import mutation from './mutations';
 
-// Schema
+// SCHEMA CAN BY ACCESSED BY A CLIENT
+// SCHEMA MANAGES QUERIES AND MUTATIONS, DESCRIBES COMPLETE SET OF POSSIBLE DATA ( OBJECTS, FIELDS, RELATIONSHIPS...)
 const schema = new GraphQLSchema({
-  query,
-  mutation
-})
+    // QUERIES ARE USED TO FETCH DATA FROM GRAPHQL ( LIKE GET IN REST API )
+    query,
+    // MUTATIONS USED TO MODIFY DATA ON THE SERVER AND GET UPDATED DATA BACK ( CREATE, UPDATE, DELETE )
+    mutation,
+});
 
-export default schema
+export default schema;
+
+// ALMOST ALL GRAPHQL TYPES DEFINED ARE OBJECTS
+// GRAPHQL ANNOTATION
