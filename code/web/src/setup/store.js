dc93ea47b39d923afa6847d5971421bd663fdc55
Ed Ignot <edignot@gmail.com>
2020-08-24 14:59:25 -0600

    Redux dev tools setup
    Clenup user Profile component
    
    Adding Logout functionality to navigation
    
    Changing button color to white

--- code/web/src/setup/store.js
+++ code/web/src/setup/store.js
@@ -1,46 +1,44 @@
 // Imports
-import { compose, combineReducers } from 'redux'
-import { createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
+import { compose, combineReducers } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 // App Imports
-import common from '../modules/common/api/state'
-import user from '../modules/user/api/state'
-import * as product from '../modules/product/api/state'
-import * as subscription from '../modules/subscription/api/state'
-import * as crate from '../modules/crate/api/state'
+import common from '../modules/common/api/state';
+import user from '../modules/user/api/state';
+import * as product from '../modules/product/api/state';
+import * as subscription from '../modules/subscription/api/state';
+import * as crate from '../modules/crate/api/state';
 
 // App Reducer
 const appReducer = combineReducers({
-  common,
-  user,
-  ...product,
-  ...subscription,
-  ...crate
-})
+    common,
+    user,
+    ...product,
+    ...subscription,
+    ...crate,
+});
 
 // Root Reducer
 export const rootReducer = (state, action) => {
-  if (action.type === 'RESET') {
-    state = undefined
-  }
+    if (action.type === 'RESET') {
+        state = undefined;
+    }
 
-  return appReducer(state, action)
-}
+    return appReducer(state, action);
+};
 
 // Load initial state from server side
-let initialState
+let initialState;
 if (typeof window !== 'undefined') {
-  initialState = window.__INITIAL_STATE__
-  delete window.__INITIAL_STATE__
+    initialState = window.__INITIAL_STATE__;
+    delete window.__INITIAL_STATE__;
 }
 
 // Store
 export const store = createStore(
-  rootReducer,
-  initialState,
-
-  compose(
-    applyMiddleware(thunk),
-  )
-)
+    rootReducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(thunk))
+);
