0fab14e0280bb66f2b500b4014904d411434c2c8
Kyle Wong <kylewong2510@gmail.com>
2020-08-25 14:14:59 -0600

    Install testing dependencies

---
+++ code/web/src/modules/user/Profile.test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Profile from './Profile'
+import Item from '../crate/Item'
+
+describe('Profile', () => {
+  it('should return true', () => {
+    expect(true).toEqual(true)
+  })
+})
