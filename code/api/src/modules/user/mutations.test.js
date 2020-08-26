9a30e4f1a87aeb8cbae0097ecf920f137713a475
Alex Pariseau <arpariseau@gmail.com>
2020-08-26 00:04:12 -0600

    Test written for updating user emails

---
+++ code/api/src/modules/user/mutations.test.js
@@ -0,0 +1,32 @@
+import request from 'supertest'
+import express from 'express'
+import graphqlHTTP from 'express-graphql'
+import schema from '../../setup/schema'
+
+describe('user mutations', () => {
+  let server;
+
+  beforeAll(() => {
+    server = express();
+    server.use('/',
+      graphqlHTTP({
+        schema: schema,
+        graphiql: false
+      })
+    );
+  })
+
+  it('changes emails', async () => {
+    const response = await request(server)
+      .get('/')
+      .send({ mutation: '{ userUpdate(id: 2, email: \"user@email.com\") { id email } }' })
+
+    console.log(response.body)
+
+    response.expect(200)
+    expect(response.body.data.user.email).toEqual("user@email.com")
+  })
+
+  // it('does not change email if it already exists')
+
+})
