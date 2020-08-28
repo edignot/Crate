79c52fb7dfbb900e90448d117462d3d772b72099
Alex Pariseau <arpariseau@gmail.com>
2020-08-28 01:05:43 -0600

    Fix for teardown, but now update not working in test - still working in development

--- code/api/src/modules/user/mutations.test.js
+++ code/api/src/modules/user/mutations.test.js
@@ -2,9 +2,11 @@ import request from 'supertest'
 import express from 'express'
 import graphqlHTTP from 'express-graphql'
 import schema from '../../setup/schema'
+import models from '../../setup/models'
 
 describe('user mutations', () => {
   let server;
+  let user;
 
   beforeAll(() => {
     server = express();
@@ -16,40 +18,46 @@ describe('user mutations', () => {
     );
   })
 
+  beforeEach(async() => {
+    user = await models.User.create({name: 'John Doe', email: "user@email.com", password: "abc123"})
+  })
+
   it('changes emails', async () => {
     const response = await request(server)
       .post('/')
       .send({ query:
-        'mutation { userUpdate(id: 2, email: "user@email.com", description: " ") { email } }'
+        `mutation { userUpdate(id: ${ user.id }, email: "email@email.com", description: " ") { email } }`
       })
-      .send({ query: '{ user(id: 2) { email description } }' })
       .expect(200)
 
-    expect(response.body.data.user.email).toEqual("user@email.com")
+    expect(user.email).toEqual("email@email.com")
   })
 
   it('does not change email if it already exists', async () => {
     const response = await request(server)
       .post('/')
       .send({ query:
-        'mutation { userUpdate(id: 2, email: "admin@crate.com", description: " ") { email } }'
+        `mutation { userUpdate(id: ${ user.id }, email: "user@crate.com", description: " ") { email } }`
       })
 
     expect(response.body.errors[0].message).toEqual(
-      'The email admin@crate.com is already registered. Please choose another.')
+      'The email user@crate.com is already registered. Please choose another.')
   })
 
   it('can add a description', async () => {
     const response = await request(server)
       .post('/')
       .send({ query:
-        `mutation { userUpdate(id: 2, email: "user@crate.com",
+        `mutation { userUpdate(id: ${ user.id }, email: "user@email.com",
         description: "A user description") { description } }`
       })
-      .send({ query: '{ user(id: 2) { description } }' })
       .expect(200)
 
-    expect(response.body.data.user.description).toEqual("A user description")
+    expect(user.description).toEqual("A user description")
+  })
+
+  afterEach(async() => {
+    return await models.User.destroy({where: { id: user.id }})
   })
 
 })
