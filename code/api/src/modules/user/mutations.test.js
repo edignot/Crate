bc227b155b62a2ef535ca3039e84866e04173306
Alex Pariseau <arpariseau@gmail.com>
2020-08-28 13:09:31 -0600

    Finally fixed testing

--- code/api/src/modules/user/mutations.test.js
+++ code/api/src/modules/user/mutations.test.js
@@ -3,10 +3,12 @@ import express from 'express'
 import graphqlHTTP from 'express-graphql'
 import schema from '../../setup/schema'
 import models from '../../setup/models'
+import connection from '../../setup/database'
+
+const params = require('../../config/params.json');
 
 describe('user mutations', () => {
   let server;
-  let user;
 
   beforeAll(() => {
     server = express();
@@ -19,45 +21,65 @@ describe('user mutations', () => {
   })
 
   beforeEach(async() => {
-    user = await models.User.create({name: 'John Doe', email: "user@email.com", password: "abc123"})
+    const admin = await models.User.create({
+      id: 1,
+      name: 'The Admin',
+      email: 'admin@crate.com',
+      password: '123456',
+      role: params.user.roles.admin,
+    })
+
+    const user = await models.User.create({
+      id: 2,
+      name: 'The User',
+      email: 'user@crate.com',
+      password: '123456',
+      role: params.user.roles.user,
+    })
   })
 
   it('changes emails', async () => {
     const response = await request(server)
       .post('/')
       .send({ query:
-        `mutation { userUpdate(id: ${ user.id }, email: "email@email.com", description: " ") { email } }`
+        `mutation { userUpdate(id: 2, email: "user@email.com", description: null) { email } }`
       })
       .expect(200)
 
-    expect(user.email).toEqual("email@email.com")
+    const test = await models.User.findByPk(2)
+    expect(test.email).toEqual("user@email.com")
   })
 
   it('does not change email if it already exists', async () => {
     const response = await request(server)
       .post('/')
       .send({ query:
-        `mutation { userUpdate(id: ${ user.id }, email: "user@crate.com", description: " ") { email } }`
+        `mutation { userUpdate(id: 2, email: "admin@crate.com", description: null) { email } }`
       })
 
     expect(response.body.errors[0].message).toEqual(
-      'The email user@crate.com is already registered. Please choose another.')
+      'The email admin@crate.com is already registered. Please choose another.')
   })
 
   it('can add a description', async () => {
     const response = await request(server)
       .post('/')
       .send({ query:
-        `mutation { userUpdate(id: ${ user.id }, email: "user@email.com",
+        `mutation { userUpdate(id: 2, email: "user@crate.com",
         description: "A user description") { description } }`
       })
       .expect(200)
 
-    expect(user.description).toEqual("A user description")
+    const test = await models.User.findByPk(2)
+    expect(test.description).toEqual("A user description")
   })
 
   afterEach(async() => {
-    return await models.User.destroy({where: { id: user.id }})
+    await models.User.destroy({where: { }})
+  })
+
+  afterAll(() => {
+    connection.close()
   })
 
 })
